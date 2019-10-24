b146c1efe778ae4742a91ba37acdef288371333f
Abrahem Gh <abrahem_gh@yahoo.com>
2019-10-24 20:12:58 +0300

    original version

---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter first number:");
+        int firstNumber=in.nextInt();
+        System.out.println("Enter second number:");
+        int secondNumber=in.nextInt();
+        System.out.println("Enter Operation:");
+        String op= in.next();
+        switch (op){
+            case "+": System.out.println("result: "+(firstNumber+secondNumber));break;
+            case "-": System.out.println("result: "+(firstNumber-secondNumber));break;
+            case "/": System.out.println("result: "+(firstNumber/secondNumber));break;
+            case "*": System.out.println("result: "+(firstNumber*secondNumber));break;
+
+        }
+        System.exit(0);
+    }
+}
